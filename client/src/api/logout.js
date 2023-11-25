6c591311f8bf120200db82a02d2f5cf5edbd593c
kirito <manhtruong@gmail.com>
2023-11-25 16:44:01 +0700

    api link

--- client/src/api/logout.js
+++ client/src/api/logout.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// export default axios.create({
+//   baseURL: "http://localhost:2020",
+// });
 export default axios.create({
-  baseURL: "http://localhost:2020",
+  baseURL: "https://ticket-do-an.vercel.app/",
 });
