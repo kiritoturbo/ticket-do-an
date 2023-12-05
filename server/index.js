9a47bacadda3a73a2624154a54db49c50498bab2
kirito <manhtruong@gmail.com>
2023-12-06 00:57:07 +0700

    socket-4

--- server/index.js
+++ server/index.js
@@ -26,7 +26,11 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 require("./db").connectMongoDb(mongoUrl);
 // Đặt múi giờ Hà Nội
